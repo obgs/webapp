014632d11634fde56084d9176ebf0790e5575262
flungloaf <flungloaf@gmail.com>
2022-08-20 21:46:17 +0300

    Add material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
