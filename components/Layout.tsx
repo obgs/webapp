2e7e316697a439579a708d74ee2695812f9dbda1
flungloaf <flungloaf@gmail.com>
2022-09-19 03:30:42 +0300

    Add graphql instrumentary

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
-import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import React, { useMemo, useState } from "react";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import LoginModal from "./auth/LoginModal";
 import useAuth from "../utils/auth/useAuth";
+import { useMeQuery } from "../graphql/generated";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   const [loginModalOpen, setLoginModalOpen] = useState(false);
 
   const { authenticated, signout } = useAuth();
 
+  const { data } = useMeQuery({
+    skip: !authenticated,
+  });
+
+  const user = useMemo(() => data?.me, [data]);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Box sx={{ flexGrow: 1 }} />
+          {user && (
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Hi, {user.email}
+            </Typography>
+          )}
           <Button
             onClick={() =>
               authenticated ? signout() : setLoginModalOpen(true)
---
+++ graphql/introspection-result.ts
@@ -0,0 +1,11 @@
+export interface PossibleTypesResultData {
+  possibleTypes: {
+    [key: string]: string[];
+  };
+}
+const result: PossibleTypesResultData = {
+  possibleTypes: {
+    Node: ["User"],
+  },
+};
+export default result;
