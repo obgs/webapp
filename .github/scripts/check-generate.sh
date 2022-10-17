ce77d9ae1218ad8e225416c449764a182d4bdefd
flungloaf <flungloaf@gmail.com>
2022-10-17 10:01:45 +0600

    Add ci code checks

---
+++ .github/scripts/check-generate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+pnpm generate
+
+if [[ "$(git status --porcelain=v2 | wc -l)" -ne 0 ]] ; then
+    echo 'You need to run "go generate" and commit the changes!'
+    git status --verbose --short
+    exit 1
+fi
