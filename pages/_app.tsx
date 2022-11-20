ebadb76e3dbe1415fe400014c2d975e472d67a1c
Rifat Kudryakov <flungloaf@gmail.com>
2022-11-20 09:40:58 +0600

    Use dark mode if its preferred in the system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,31 @@
 import "../styles/globals.css";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider, useMediaQuery } from "@mui/material";
 import type { AppProps } from "next/app";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { SnackbarProvider } from "notistack";
+import { useMemo } from "react";
 
 import Layout from "../components/nav/Layout";
 import ApolloProvider from "../utils/apollo/provider";
 import AuthProvider from "../utils/auth/provider";
 import UserProvider from "../utils/user/provider";
 
-const theme = createTheme();
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider>
