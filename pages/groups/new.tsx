801b5278342a3542aa9e261febee357823e27fb0
flungloaf <flungloaf@gmail.com>
2022-11-23 16:03:19 +0600

    Move group creation form into it's own component

--- pages/groups/new.tsx
+++ pages/groups/new.tsx
@@ -1,43 +1,14 @@
-import { LoadingButton } from "@mui/lab";
-import {
-  Box,
-  Container,
-  FormControlLabel,
-  FormHelperText,
-  FormLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-  Stack,
-  Switch,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useFormik } from "formik";
+import { Container } from "@mui/material";
 import { useRouter } from "next/router";
 import { useSnackbar } from "notistack";
-import React, { useEffect } from "react";
-import * as yup from "yup";
+import React from "react";
 
-import { ImageInput, useImageInput } from "../../components/ImageInput";
+import GroupForm, { SubmitCallback } from "../../components/groups/GroupForm";
 import {
-  GroupMembershipRole,
-  GroupSettingsJoinPolicy,
-  GroupSettingsVisibility,
   SearchGroupsDocument,
   useCreateGroupMutation,
 } from "../../graphql/generated";
 import useSnackbarError from "../../utils/apollo/useSnackbarError";
-
-const validationSchema = yup.object({
-  name: yup.string().required("Name is required"),
-  description: yup.string(),
-  logoURL: yup.string().required("Logo is required"),
-  visibility: yup.mixed().oneOf(Object.values(GroupSettingsVisibility)),
-  joinPolicy: yup.mixed().oneOf(Object.values(GroupSettingsJoinPolicy)),
-  minimumRoleToInvite: yup.mixed().oneOf(Object.values(GroupMembershipRole)),
-});
 
 const CreateGroup = () => {
   const [createGroup, { loading, error }] = useCreateGroupMutation();
@@ -45,172 +16,39 @@ const CreateGroup = () => {
 
   const { enqueueSnackbar } = useSnackbar();
   const router = useRouter();
-  const { handleChange, setFieldValue, handleSubmit, values, errors, touched } =
-    useFormik({
-      validationSchema,
-      initialValues: {
-        name: "",
-        description: "",
-        logoURL: "",
-        visibility: GroupSettingsVisibility.Public,
-        joinPolicy: GroupSettingsJoinPolicy.Open,
-        minimumRoleToInvite: GroupMembershipRole.Member,
-      },
-      onSubmit: async ({
+
+  const onSubmit: SubmitCallback = async ({
+    name,
+    description,
+    logoURL,
+    visibility,
+    joinPolicy,
+    minimumRoleToInvite,
+  }) => {
+    await createGroup({
+      variables: {
         name,
         description,
-        logoURL,
+        logoUrl: logoURL,
         visibility,
         joinPolicy,
         minimumRoleToInvite,
-      }) => {
-        await createGroup({
-          variables: {
-            name,
-            description,
-            logoUrl: logoURL,
-            visibility,
-            joinPolicy,
-            minimumRoleToInvite,
-          },
-          // updating the cache manually would be harder, so we just refetch the whole list
-          refetchQueries: [
-            {
-              query: SearchGroupsDocument,
-              variables: { where: {}, first: 10 },
-            },
-          ],
-        });
-        enqueueSnackbar(`Group ${name} created.`, { variant: "success" });
-        router.push("/groups");
       },
+      // updating the cache manually would be harder, so we just refetch the whole list
+      refetchQueries: [
+        {
+          query: SearchGroupsDocument,
+          variables: { where: {}, first: 10 },
+        },
+      ],
     });
-
-  const { onChange, url: logoURL } = useImageInput();
-
-  useEffect(() => {
-    if (values.logoURL === logoURL) return;
-    setFieldValue("logoURL", logoURL);
-  }, [logoURL, setFieldValue, values.logoURL]);
+    enqueueSnackbar(`Group ${name} created.`, { variant: "success" });
+    router.push("/groups");
+  };
 
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={2}>
-          <Typography variant="h4">Create a new group</Typography>
-
-          <Typography variant="body1">General</Typography>
-          <Stack direction="row" spacing={2}>
-            <Box>
-              <FormLabel>Logo</FormLabel>
-              <ImageInput existingAvatarURL={logoURL} onChange={onChange} />
-              {touched.logoURL && errors.logoURL && (
-                <FormHelperText error>{errors.logoURL}</FormHelperText>
-              )}
-            </Box>
-            <Stack spacing={2}>
-              <TextField
-                label="Name"
-                value={values.name}
-                onChange={handleChange}
-                name="name"
-                error={touched.name && !!errors.name}
-                helperText={touched.name && errors.name}
-              />
-              <TextField
-                multiline
-                label="Description (optional)"
-                value={values.description}
-                onChange={handleChange}
-                name="description"
-              />
-            </Stack>
-          </Stack>
-
-          <Typography variant="body1">Settings</Typography>
-          <FormLabel>Visibility</FormLabel>
-          <FormHelperText>
-            If the group is public, it can appear in searches. Private groups
-            are only visible to their members.
-          </FormHelperText>
-          <FormControlLabel
-            label="Public"
-            control={
-              <Switch
-                checked={values.visibility === GroupSettingsVisibility.Public}
-                onChange={(e) => {
-                  setFieldValue(
-                    "visibility",
-                    e.target.checked
-                      ? GroupSettingsVisibility.Public
-                      : GroupSettingsVisibility.Private
-                  );
-                }}
-              />
-            }
-          />
-
-          <FormLabel>How to join the group?</FormLabel>
-          <RadioGroup
-            value={values.joinPolicy}
-            onChange={handleChange}
-            name="joinPolicy"
-          >
-            <FormControlLabel
-              value={GroupSettingsJoinPolicy.Open}
-              label="Anyone can join at any time"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value={GroupSettingsJoinPolicy.ApplicationOnly}
-              label="By application"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value={GroupSettingsJoinPolicy.InviteOnly}
-              label="Members of this group can invite other users"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value={GroupSettingsJoinPolicy.InviteOrApplication}
-              label="Application or invite"
-              control={<Radio />}
-            />
-          </RadioGroup>
-
-          {(values.joinPolicy === GroupSettingsJoinPolicy.InviteOnly ||
-            values.joinPolicy ===
-              GroupSettingsJoinPolicy.InviteOrApplication) && (
-            <>
-              <FormLabel>
-                Minimum role to invite other users to the group
-              </FormLabel>
-              <Box>
-                <Select
-                  value={values.minimumRoleToInvite}
-                  name="minimumRoleToInvite"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={GroupMembershipRole.Member}>Member</MenuItem>
-                  <MenuItem value={GroupMembershipRole.Admin}>Admin</MenuItem>
-                  <MenuItem value={GroupMembershipRole.Owner}>Owner</MenuItem>
-                </Select>
-              </Box>
-            </>
-          )}
-        </Stack>
-
-        <Stack
-          direction="row"
-          justifyContent="flex-end"
-          mt={2}
-          alignSelf="flex-end"
-        >
-          <LoadingButton loading={loading} variant="contained" type="submit">
-            Create
-          </LoadingButton>
-        </Stack>
-      </form>
+      <GroupForm onSubmit={onSubmit} loading={loading} buttonLabel="Create" />
     </Container>
   );
 };
