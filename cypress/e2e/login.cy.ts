ad4acf07ed0deea64c7a228d118ce37c0430c967
worstcop <renookun@gmail.com>
2023-02-14 19:35:53 +0400

    fix linter violations

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,19 +1,24 @@
 const goToLogIn = () => {
-    cy.visit("/");
-    cy.contains("Log in").click();
+  cy.visit("/");
+  cy.contains("Log in").click();
 };
 
 const logIn = (email: string, password: string) => {
-    cy.get("input[type=email]").type(email);
-    cy.get("input[type=password]").type(password);
-    cy.get(".MuiBox-root").contains("Not registered?").parent().find("button").contains("Log in").click();
+  cy.get("input[type=email]").type(email);
+  cy.get("input[type=password]").type(password);
+  cy.get(".MuiBox-root")
+    .contains("Not registered?")
+    .parent()
+    .find("button")
+    .contains("Log in")
+    .click();
 };
 
 describe("Log in", () => {
-    it("login in the user", () => {
-        goToLogIn();
-        logIn("rifat@gmail.com", "123456");
-        // On success you are redirected to the home page
-        cy.contains("My players");
-    });
+  it("login in the user", () => {
+    goToLogIn();
+    logIn("rifat@gmail.com", "123456");
+    // On success you are redirected to the home page
+    cy.contains("My players");
+  });
 });
