2c45f8d43f0d1f589502f7c4d2b1dc6103545ad2
worstcop <125278624+worstcop@users.noreply.github.com>
2023-02-14 20:07:55 +0400

    add login tests (#25)

---
+++ webapp/cypress/e2e/login.cy.ts
@@ -0,0 +1,38 @@
+const goToLogIn = () => {
+  cy.visit("/");
+  cy.contains("Log in").click();
+};
+
+const logIn = (email: string, password: string) => {
+  cy.get("input[type=email]").type(email);
+  cy.get("input[type=password]").type(password);
+  cy.get(".MuiBox-root")
+    .contains("Not registered?")
+    .parent()
+    .find("button")
+    .contains("Log in")
+    .click();
+};
+
+describe("Log in", () => {
+  it("login in the user", () => {
+    goToLogIn();
+    logIn("test-user-1@obgs.app", "12345");
+    // On success you are redirected to the home page
+    cy.contains("My players");
+  });
+
+  it("login pass fail", () => {
+    goToLogIn();
+    logIn("test-user-1@obgs.app", "1234");
+    // On success you get the message
+    cy.contains("Invalid email or password");
+  });
+
+  it("login user fail", () => {
+    goToLogIn();
+    logIn("admin@admin.com", "12345");
+    // On success you get message
+    cy.contains("User not found");
+  });
+});
