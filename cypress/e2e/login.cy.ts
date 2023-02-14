0bfd3e9abd2dcd4d812213b5ece67e2052c6bb64
worstcop <renookun@gmail.com>
2023-02-14 20:04:43 +0400

    changed on test user info

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -17,21 +17,21 @@ const logIn = (email: string, password: string) => {
 describe("Log in", () => {
   it("login in the user", () => {
     goToLogIn();
-    logIn("rifat@gmail.com", "123456");
+    logIn("test-user-1@obgs.app", "12345");
     // On success you are redirected to the home page
     cy.contains("My players");
   });
 
   it("login pass fail", () => {
     goToLogIn();
-    logIn("rifat@gmail.com", "12345");
+    logIn("test-user-1@obgs.app", "1234");
     // On success you get the message
     cy.contains("Invalid email or password");
   });
 
   it("login user fail", () => {
     goToLogIn();
-    logIn("admin@admin.com", "123456");
+    logIn("admin@admin.com", "12345");
     // On success you get message
     cy.contains("User not found");
   });
