0324f86ff7b58abd389326ebf4ebed58c482e6f3
worstcop <renookun@gmail.com>
2023-03-02 06:51:10 +0400

    added game creation test

---
+++ cypress/e2e/games_control.cy.ts
@@ -0,0 +1,41 @@
+import { logInTestUser1 } from "./utils";
+
+const addGameInfo = (gameName: string, gameDesc: string, boardLink: string) => {
+  cy.get("input").first().type(gameName);
+  cy.get("textarea").first().type(gameDesc);
+  cy.get("input[name=boardgamegeekURL]").type(boardLink);
+};
+
+const addStatsInfo = (statName: string, statDesc: string) => {
+  cy.get(".MuiCardContent-root")
+    .children()
+    .find("input")
+    .first()
+    .type(statName);
+  cy.get(".MuiCardContent-root")
+    .children()
+    .find("textarea")
+    .first()
+    .type(statDesc);
+};
+
+describe("Games control", () => {
+  it("creates new game", () => {
+    //Creating a game with standart 10 players and 1 numeric stat
+    cy.visit("/");
+    logInTestUser1();
+    cy.visit("games");
+    cy.get("button").contains("Create new game").click();
+    cy.visit("games/new");
+    addGameInfo(
+      "test-game-01",
+      "Test name info",
+      "https://boardgamegeek.com/boardgame/316554/dune-imperium"
+    );
+    addStatsInfo("test-stat-01", "Test stat info");
+    cy.get("button").contains("Create").click();
+    //On success test-user-01 find test-game-01 on Games page
+    cy.visit("games");
+    cy.contains("test-game-01");
+  });
+});
