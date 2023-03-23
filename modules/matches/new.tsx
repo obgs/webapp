1b516d5807b5c9e79bbbb307d6c41f54f3a43e7f
Rifat Kudryakov <flungloaf@gmail.com>
2023-03-23 17:32:07 +0400

    Mobile improvements (#39)
    * Hide the drawer on mobile
    
    * Implement a list view for creating a match

--- modules/matches/new.tsx
+++ modules/matches/new.tsx
@@ -1,8 +1,15 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import ListIcon from "@mui/icons-material/List";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TableViewIcon from "@mui/icons-material/TableView";
 import { LoadingButton } from "@mui/lab";
 import {
+  Box,
   Button,
   Container,
+  Divider,
+  Grid,
+  IconButton,
   Stack,
   Step,
   StepLabel,
@@ -12,6 +19,11 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Toolbar,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { useRouter } from "next/router";
 import { useSnackbar } from "notistack";
@@ -34,6 +46,7 @@ import { useSnackbarError } from "utils/apollo";
 import { useUser } from "utils/user";
 
 const steps = ["Select game", "Enter stats"];
+const viewTypes = ["table", "list"] as const;
 
 const CreateMatch = () => {
   const { user } = useUser();
@@ -44,6 +57,15 @@ const CreateMatch = () => {
     defaultValues: {},
     resolver: zodResolver(schema),
   });
+
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [viewType, setViewType] = useState<typeof viewTypes[number]>(
+    mobile ? "list" : "table"
+  );
+  useEffect(() => {
+    setViewType(mobile ? "list" : "table");
+  }, [mobile]);
 
   const { setValue, handleSubmit } = form;
 
@@ -148,46 +170,112 @@ const CreateMatch = () => {
       )}
       {activeStep === 1 && (
         <FormProvider {...form}>
+          <Toolbar>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{ flexGrow: 1 }}
+              justifyContent="flex-end"
+            >
+              <Tooltip title="List view">
+                <IconButton onClick={() => setViewType("list")}>
+                  <ListIcon
+                    color={viewType === "list" ? "primary" : "inherit"}
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Table view">
+                <IconButton onClick={() => setViewType("table")}>
+                  <TableViewIcon
+                    color={viewType === "table" ? "primary" : "inherit"}
+                  />
+                </IconButton>
+              </Tooltip>
+              <Divider orientation="vertical" flexItem />
+              <Tooltip title="Add player">
+                <IconButton onClick={() => setShowFindPlayer(true)}>
+                  <PersonAddIcon />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+          </Toolbar>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Table sx={{ mb: 2 }}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Player</TableCell>
-                  {game?.statDescriptions.map((d) => (
-                    <TableCell key={d.id}>{d.name}</TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {players.map((player) => (
-                  <TableRow hover key={player.id}>
-                    <TableCell>
-                      {player.id === user?.mainPlayer?.id
-                        ? "You"
-                        : player.name || player.owner?.name}
-                    </TableCell>
+            {viewType === "table" ? (
+              <Table sx={{ mb: 2 }}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Player</TableCell>
                     {game?.statDescriptions.map((d) => (
-                      <TableCell key={d.id}>
-                        <StatInput statDescription={d} playerId={player.id} />
-                      </TableCell>
+                      <TableCell key={d.id}>{d.name}</TableCell>
                     ))}
                   </TableRow>
-                ))}
-                <TableRow>
-                  <TableCell
-                    align="right"
-                    colSpan={(game?.statDescriptions.length || 0) + 1}
-                  >
-                    <Button
-                      variant="contained"
-                      onClick={() => setShowFindPlayer(true)}
-                    >
-                      Add player
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+                </TableHead>
+                <TableBody>
+                  {players.map((player) => (
+                    <TableRow hover key={player.id}>
+                      <TableCell>
+                        {player.id === user?.mainPlayer?.id
+                          ? "You"
+                          : player.name || player.owner?.name}
+                      </TableCell>
+                      {game?.statDescriptions.map((d) => (
+                        <TableCell key={d.id}>
+                          <StatInput statDescription={d} playerId={player.id} />
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            ) : (
+              game?.statDescriptions.map((stat, i) => (
+                <Box key={stat.id} sx={{ mb: 2 }}>
+                  <Typography variant="h6" sx={{ mb: 2 }}>
+                    {stat.name}
+                  </Typography>
+                  <Stack spacing={2}>
+                    {players.map((player) => (
+                      <Grid
+                        key={player.id}
+                        container
+                        sx={{
+                          alignItems: "center",
+                        }}
+                      >
+                        <Grid item xs={6}>
+                          <Box
+                            sx={{
+                              flex: 1,
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "flex-end",
+                              height: "100%",
+                              mr: 2,
+                            }}
+                          >
+                            <Typography variant="body1">
+                              {player.id === user?.mainPlayer?.id
+                                ? "You"
+                                : player.name || player.owner?.name}
+                            </Typography>
+                          </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <StatInput
+                            statDescription={stat}
+                            playerId={player.id}
+                          />
+                        </Grid>
+                      </Grid>
+                    ))}
+                  </Stack>
+                  {i !== game.statDescriptions.length - 1 && (
+                    <Divider sx={{ mt: 2, mb: 2 }} />
+                  )}
+                </Box>
+              ))
+            )}
+
             <Stack direction="row" justifyContent="space-between">
               <Button onClick={() => setActiveStep(0)}>Back</Button>
               <LoadingButton
