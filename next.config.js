ace67f320336af4085f48fd4929585c0618f728b
flungloaf <flungloaf@gmail.com>
2023-03-25 16:08:23 +0400

    Add a favicon and a logo to the appbar

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
     domains: ["localhost"],
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
