c12ab9691951cf71d8487c2dcf1ffa26b95c86b8
Rifat Kudriakov <flungloaf@gmail.com>
2024-06-29 21:15:00 +0400

    Move matches page to /app

--- app/matches/components/List.tsx
+++ app/matches/components/List.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import FilterListIcon from "@mui/icons-material/FilterList";
 import {
   Button,
---
+++ app/matches/page.tsx
@@ -0,0 +1,5 @@
+import List from "@/matches/components/List";
+
+const Browse = () => <List />;
+
+export default Browse;
