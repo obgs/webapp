b4cd8560e0148d51464af88875ed500f829117dd
Rifat Kudriakov <flungloaf@gmail.com>
2024-06-29 21:23:41 +0400

    Fix match lists navigation import

--- app/matches/components/List.tsx
+++ app/matches/components/List.tsx
@@ -17,7 +17,7 @@ import {
   Toolbar,
 } from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useMemo, useState } from "react";
 
 import {
