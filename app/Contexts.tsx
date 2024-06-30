f5f7bb141095828e887051aa3c9ac4630c2fb16e
Rifat Kudriakov <flungloaf@gmail.com>
2024-07-01 00:49:45 +0400

    Always use dark mode

--- app/Contexts.tsx
+++ app/Contexts.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useMediaQuery } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { SnackbarProvider } from "notistack";
-import { PropsWithChildren, useMemo } from "react";
+import { PropsWithChildren } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -12,19 +11,13 @@ import { AuthProvider } from "modules/auth";
 import { ApolloProvider } from "utils/apollo";
 import { UserProvider } from "utils/user";
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const Contexts: React.FC<PropsWithChildren> = ({ children }) => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
-  );
-
   return (
     <AppRouterCacheProvider>
       <ThemeProvider theme={theme}>
